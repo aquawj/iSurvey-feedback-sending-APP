a26227d75cc51d411e2f38162c564cb38e7844dc
jane <jane@JiadeMacBook-Pro.local>
2018-04-18 20:57:22 -0700

    modified Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import Payments from './Payments';
+
 
 class Header extends Component {
     renderContent(){
